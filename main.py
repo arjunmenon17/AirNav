6c9d11b65ba34a585af9794e490275ef3c0c3f38
Arjun Menon <arjun.mnn17@gmail.com>
2023-03-29 18:40:15 -0400

    main v1

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class RunSimulation:
+    def __init__(self):
+        self.canvas = Canvas(root, width=798, height=462)
+        self.canvas.config(background="snow2")
+        self.canvas.place(x=512, y=288, anchor="center")
+        self.canvas.focus_set()
 
 
-# Press the green button in the gutter to run the script.
+def initializeProgram():
+    gameCurrent = RunSimulation()
+
+
+def startGraphics():
+    root = Tk()
+    root.title("Air Nav")
+    root.geometry("1024x576")
+    root.resizable(width=False, height=False)
+    root.configure(bg="gold")
+
+    titleLabel = Label(root, bg="gold", text="Air Nav", font=("Harlow Solid Italic", 100))
+    titleLabel.place(x=512, y=150, anchor="center")
+    authorLabel = Label(root, bg="gold", text="Designed by Arjun, Rohan, Hadi & Azlan", font=("Dubai Light", 28))
+    authorLabel.place(x=512, y=250, anchor="center")
+
+    startButton = Button(root, text="Start", borderwidth=10, bg="gold", width=15, font=("Helvetica", 16, "bold"), command=lambda: initializeProgram())
+    startButton.place(x=367, y=420, anchor="center")
+
+    quitButton = Button(root, text="Quit", borderwidth=10, bg="gold",  width=15, font=("Helvetica", 16, "bold"), command=lambda: root.destroy())
+    quitButton.place(x=642, y=420, anchor="center")
+
+    mainloop()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    startGraphics()
