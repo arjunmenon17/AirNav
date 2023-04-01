9c40b09b0963ba9126d9e48933a23f6002bac73c
azlannaeem <97074156+azlannaeem@users.noreply.github.com>
2023-04-01 18:53:33 -0400

    Update data.py

--- data.py
+++ data.py
@@ -11,13 +11,9 @@ def get_dataframe1(csv_file: str) -> pd.DataFrame:
         - csv_file in os.getcwd()
         - csv_file is a valid dataset
     """
-    
-    airport_cols = ['3 Char Code', 'Facility Name', 'Lat', 'Long',
-                    'rand', 'rand1', 'rand2', 'rand3', 'rand4', 'rand5']
-    df_airports = pd.read_csv('airports.csv', skiprows=1, names=airport_cols)
-
-    df = df_airports.dropna()
-    df1 = df[['3 Char Code', 'Lat', 'Long']]
+    df_airports = pd.read_csv('airports.csv')
+    df = df_airports.dropna() # Drops rows with NaN values
+    df1 = df[['3 Char Code', 'Lat', 'Long', 'Airport Name', 'City', 'Country']]
 
     return df1
     
@@ -31,10 +27,10 @@ def get_dataframe2(csv_file: str) -> pd.DataFrame:  # list[list[str, str]]
         - csv_file is a valid dataset
     """
     route_cols = ['Airline', 'Airline ID', 'Source', 'Source Airport ID',
-                'Dest', 'Dest Airport ID', 'Codeshare', 'Stops', 'equipment']
-    routes_df = pd.read_csv(csv_file, skiprows=1, names=route_cols)
+                'Dest', 'Dest Airport ID', 'Codeshare', 'Stops', 'equipment'] # Sets new column names for the dataframe
+    routes_df = pd.read_csv('flight_routes.csv', names=route_cols, skiprows=1, low_memory=False) # Has too many columns
 
-    df = routes_df.dropna()
+    df = routes_df.dropna() # Drops rows with NaN values
     df2 = df[['Source', 'Dest']]
 
     return df2
@@ -44,7 +40,7 @@ def get_location(airport_key: str) -> tuple[float, float]:
     Returns the latitude/longtidude of a given airport.
 
     Preconditions:
-    - len(airport_key) == 3
+        - len(airport_key) == 3
     """
     df_airports = get_dataframe1('airports.csv')
     for ind in df_airports.index:
@@ -57,8 +53,7 @@ def get_destinations(airport_key: str) -> list[tuple[str, tuple[float, float]]]:
     Returns the list of destinations with their airport codes and latitude/longitude for the given airport.
 
     Preconditions:
-    - len(airport_key) == 3
-    - 
+        - len(airport_key) == 3
     """
     df_routes = get_dataframe2('flight_routes.csv')
     df_airports = get_dataframe1('airports.csv')
@@ -66,12 +61,12 @@ def get_destinations(airport_key: str) -> list[tuple[str, tuple[float, float]]]:
         if df_routes['3 Char Code'][ind] == airport_key:
             return [(route[1], route[2]) for route in df_routes if df_routes[0] == df_airports[1]]
 
-def get_airports() -> list[str]:
+def get_airports() -> list[tuple[str]]:
     """
-    Returns the list of all the airport codes.
+    Returns the list of tuples containing every airport's name, country, city, and code.
     """
     df_airports = get_dataframe1('airports.csv')
     airports = []
     for ind in df_airports.index:
-        airports.append(df_airports['3 Char Code'][ind])
+        airports.append((df_airports['3 Char Code'][ind], df_airports['Airport Name'][ind], df_airports['Country'][ind], df_airports['City'][ind]))
     return airports
